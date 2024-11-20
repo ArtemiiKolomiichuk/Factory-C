2d436703fdf4375b474c3250cfd2488167d749f5
Mykhailo Shevchenko <muhailo11111@gmail.com>
2024-11-20 19:23:26 +0200

    Add some ui to maingame scene

--- Factory C/Assets/Scripts/UI/GameOverUIController.cs
+++ Factory C/Assets/Scripts/UI/GameOverUIController.cs
@@ -43,6 +43,7 @@ public class GameOverUIController : MonoBehaviour
     }
 
     private void GoToMainMenu() {
+        LobbyController.Instance.LeaveLobby();
         SceneManager.LoadScene(TargetScene);
     }
 
